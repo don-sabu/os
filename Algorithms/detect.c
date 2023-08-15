d701cc34637935a1b2baf32140076fad95feb1c7
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2023-08-16 01:15:19 +0530

    Update detect.c
    Code is simplified

--- Algorithms/detect.c
+++ Algorithms/detect.c
@@ -1,76 +1,66 @@
 #include<stdio.h>
-static int mark[20];
-int i,j,np,nr;
+int requestLessThanWork(int r, int work[], int request[])
+{
+    for(int i=0;i<r;i++)
+        if(work[i]<request[i])
+            // If the any of the request is greater than work then the function exits from here returning false.
+            return 0;
+    // If there is no request greater than work then this method returns true.
+    return 1;
+}
+int checkDeadlock(int p, int mark[])
+{
+    for(int i=0;i<p;i++)
+        if(mark[i] == 0)
+            // If any one process is marked as false then this method ends here returning false.
+            return 0;
+    // If there is no process marked as false then this method returns true.        
+    return 1;
+}
 int main()
 {
-	int alloc[10][10],request[10][10],avail[10],r[10],w[10];
-	printf("Enter the no of process: ");
-	scanf("%d",&np);
-	printf("Enter the no of resources: ");
-	scanf("%d",&nr);
-	for(i=0;i<nr;i++)
-	{
-		printf("Total Amount of the Resource R%d: ",i+1);
-		scanf("%d",&r[i]);
-	}
-	printf("Enter the request matrix:\n");
-	for(i=0;i<np;i++)
-		for(j=0;j<nr;j++)
-			scanf("%d",&request[i][j]);
-	printf("Enter the allocation matrix:\n");
-	for(i=0;i<np;i++)
-		for(j=0;j<nr;j++)
-			scanf("%d",&alloc[i][j]);
-	for(j=0;j<nr;j++)
-	{
-		avail[j]=r[j];
-		for(i=0;i<np;i++)
-			avail[j]-=alloc[i][j];
-	}
-	for(i=0;i<np;i++)
-	{
-		int count=0;
-		for(j=0;j<nr;j++)
-		{
-            if(alloc[i][j]==0)
-                count++;
-            else
-                break;
-		}
-		if(count==nr)
-			mark[i]=1;
-	}
-	for(j=0;j<nr;j++)
-        w[j]=avail[j];
-	for(i=0;i<np;i++)
-	{
-		int canbeprocessed=0;
-		if(mark[i]!=1)
-		{
-			for(j=0;j<nr;j++)
+    int p, r;
+    printf("Enter the number of processes: ");
+    scanf("%d",&p);
+    printf("Enter the number of resources: ");
+    scanf("%d",&r);
+    int avail[r], alloc[p][r], request[p][r];
+    int work[r], mark[p];
+    printf("Enter the available number of instances for each resource:\n");
+    for(int i=0;i<r;i++)
+    {
+        printf("Enter no of instance for resource R%d: " ,i+1);
+        scanf("%d", &avail[i]);
+        // Initializing work to available resources
+        work[i] = avail[i];
+        // Marking each process as not processed
+        mark[i] = 0;
+    }
+    // Reading number of resources allocated for each process
+    printf("Enter the allocation matrix:\n");
+    for(int i=0;i<p;i++)
+        for(int j=0;j<r;j++)
+            scanf("%d",&alloc[i][j]);
+    // Reading the number of resources requested by each process
+    printf("Enter the request matrix:\n");
+    for(int i=0;i<p;i++)
+        for(int j=0;j<r;j++)
+            scanf("%d",&request[i][j]);
+    for(int i=0;i<p;i++)
+        if(!mark[i])
+            if(requestLessThanWork(r, work, request[i]))
             {
-                if(request[i][j]<=w[j])
-                    canbeprocessed=1;
-				else
-				{
-					canbeprocessed=0;
-					break;
-				}
-			}
-			if(canbeprocessed)
-			{
-				mark[i]=1;
-				for(j=0;j<nr;j++)
-					w[j]+=alloc[i][j];
-			}
-		}
-	}
-	int deadlock=0;
-	for(i=0;i<np;i++)
-		if(mark[i]!=1)
-			deadlock=1;
-	if(deadlock)
-		printf("Deadlock detected\n");
-	else
-		printf("No Deadlock possible\n");
+                for(int j=0;j<r;j++)
+                    work[j] += alloc[i][j];
+                    // Adding allocated resources to the available work.
+                mark[i] = 1;
+                // Marking the process as can be processed.
+                i = -1;
+                // If a process is marked then we need to start checking again from the beginning.
+                // Since only unmarked processed are checked, no infinite loop occurs.
+            }
+    if(checkDeadlock(p, mark))
+        printf("No Deadlock Detected\n");
+    else
+        printf("Deadlock Detected\n");
 }
